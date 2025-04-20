c16d6e05c2018672ca50b64bc71aa64d57c489fd
Mr uprizing <uprizingfaze@gmail.com>
2025-04-20 11:57:30 -0500

    Fisrt Commit

---
+++ app/(app)/page.tsx
@@ -0,0 +1,10 @@
+import { CardComponent } from "./components/card";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Ecommerce</div>
+      <CardComponent />
+    </div>
+  );
+}
